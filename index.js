2e9ab2c37850d1fdfe5bd19d0020f24f293e520b
Rugved Chavan <rugvedchavan19@gmail.com>
2025-06-26 18:14:55 +0530

    Set up Quill

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
   const editor = document.querySelector('.Editor');
   const count = document.querySelector('.word-count');
 
-  editor.addEventListener('input', () => {
-    const words = editor.value.trim().split(/\s+/).filter(Boolean);
-    count.textContent = `word count: ${words.length}`;
-  });
-  editor.addEventListener('input',() => {
-    editor.style.height='auto';
-    editor.style.height=editor.scrollHeight +"px";
-  });
+ 
+
+const quill = new Quill('#quill-editor', {
+  theme: 'snow',
+  modules: {
+    toolbar: false
+  }
+});
